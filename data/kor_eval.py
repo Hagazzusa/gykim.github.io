a2eb1aba67442d84032255427eea1c6d6cc1665e
Hagazzusa <uhi7074@gmail.com>
2017-06-07 16:39:06 +0900

    update

--- data/kor_eval.py
+++ data/kor_eval.py
@@ -32,17 +32,33 @@ def calc(pred, ans, top=20):
     fp = 0.
     fn = 0.
     tn = 0.
+    
+    all_ap = 0.0
     for idx in range(0, 20):
-        for prediction in pred[idx][:top]:
-            if prediction in ans[idx]: tp += 1
+        cur_tp = 0.0
+        temp = 0.
+        ap = 0.
+        last_temp = 0.
+        if top != -1 : pr_temp = pred[idx][:top]
+        else : pr_temp = pred[idx]
+        for prediction in pr_temp:
+            temp += 1
+            if prediction in ans[idx]: 
+                tp += 1
+                cur_tp += 1
+                ap += cur_tp/temp
+                last_temp = temp
             else: fp += 1
+        if last_temp != 0: all_ap += ap/last_temp
         for answer in ans[idx]:
-            if answer not in pred[idx][:top]: fn += 1
+            if answer not in pr_temp: fn += 1
+        
+    all_ap /= 20
             
     precision = tp/(tp+fp)
     recall = tp/(tp+fn)
     
-    return precision, recall
+    return precision, recall, all_ap
             
 if __name__ == '__main__':
     pred = [ [] for i in range(20)]
@@ -50,7 +66,10 @@ if __name__ == '__main__':
         for line in f:
             temp = line.split()
             pred[int(temp[0])-1].append(int(temp[2]))
-    p, r = calc(pred, answer, 20)
+    p, r, m = calc(pred, answer, 20)
+    _, _, all_m = calc(pred, answer, -1)
     print "top 20 precision : ", p
     print "top 20 recall : ", r
+    print "top 20 MAP(Mean Average Precision) : ", m
+    print "All MAP : ", all_m
             
